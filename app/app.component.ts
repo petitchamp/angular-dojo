60b063b79d6d052734b0767670c02e4990e03d2d
SONG Ke <petitchamp@gmail.com>
2016-08-10 01:24:09 +0200

    QUICKSTART

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by KE on 2016/8/10.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
+
